10226ef4423ff6dee272e288e5c9181124e8a03d
rohith1999 <rohithsuri3@gmail.com>
2022-02-03 14:40:55 +0530

    Keyclock oauth2 Authorization

---
+++ src/main/java/com/mithran/seek/authcode/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.mithran.seek.authcode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	
+}
